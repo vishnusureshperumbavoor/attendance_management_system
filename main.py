30b695d98645956185b33f271412774b9cd54125
= <vishnusureshperumbavoor@gmail.com>
2022-10-13 17:35:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import face_recognition
+import cv2
+import numpy as np
+from datetime import datetime
+video_capture = cv2.VideoCapture(0)
+
+elon_image = face_recognition.load_image_file("images/Elon.jpg")
+elon_face_encoding = face_recognition.face_encodings(elon_image)[0]
+
+vsp_image = face_recognition.load_image_file("images/VSP.jpg")
+vsp_face_encoding = face_recognition.face_encodings(vsp_image)[0]
+
+known_face_encodings = [
+    elon_face_encoding,
+    vsp_face_encoding,
+]
+known_face_names = [
+    "Elon",
+    "VSP",
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+def markAttendance(name):
+        with open('attendance.csv','r+') as f:
+            myDataList = f.readlines()
+            nameList = []
+            for line in myDataList:
+                entry = line.split(',')
+                nameList.append(entry[0])
+            if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime('%H:%M:%S')
+                f.writelines(f'\n{name},{dtString}')
+
+
+while True:
+    ret, frame = video_capture.read()
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            print(best_match_index)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                markAttendance(name)
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
